84bead87fc1e5ba0943673dbd80a72aa7b46ce0f
Yudai Tanaka <ytnk531@users.noreply.github.com>
2019-01-24 08:36:45 +0900

    カラム名変わったので修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #
-User.create!(name: 'Admin1', emailAddress: 'admin1@example.com', password: 'test', password_confirmation: "test",role: 'admin')\
-      .create_profile!(message: '管理者です。', github_url: 'ytnk531')\
-      .work_experiences.create(company_name: '毎朝新聞株式会社', work_start: Date.new)
-User.create!(name: 'Admin2', emailAddress: 'admin2@example.com', password: 'test', password_confirmation: 'test',role: 'admin')\
-      .create_profile!(message: '管理者です。', github_url: 'ytnk531')\
-      .work_experiences.create(company_name: '毎朝新聞株式会社', work_start: Date.new)
-User.create!(name: 'User1', emailAddress: 'user1@example.com', password: 'test', password_confirmation: 'test',role: 'user')\
-      .create_profile!(message: 'ユーザーです。', github_url: 'ytnk531')\
-      .work_experiences.create(company_name: '毎朝新聞株式会社', work_start: Date.new)
-User.create!(name: 'User2', emailAddress: 'user2@example.com', password: 'test', password_confirmation: 'test',role: 'user')\
-      .create_profile!(message: 'ユーザーです。', github_url: 'ytnk531')\
-      .work_experiences.create(company_name: '毎朝新聞株式会社', work_start: Date.new)
-User.create!(name: 'User3', emailAddress: 'user3@example.com', password: 'test', password_confirmation: 'test',role: 'user')\
-      .create_profile!(message: 'ユーザーです。', github_url: 'ytnk531')\
-      .work_experiences.create(company_name: '毎朝新聞株式会社', work_start: Date.new)
-User.create!(name: 'User4', emailAddress: 'user4@example.com', password: 'test', password_confirmation: 'test',role: 'user')\
-      .create_profile!(message: 'ユーザーです。', github_url: 'ytnk531')\
-      .work_experiences.create(company_name: '毎朝新聞株式会社', work_start: Date.new)
+User.create!(name: 'Admin1', email_address: 'admin1@example.com', password: 'test', password_confirmation: "test",role: 'admin')\
+      .create_profile!(message: '管理者です。', github_id: 'ytnk531')\
+      .work_experiences.create(company_name: '毎朝新聞株式会社', work_start_on: Date.new)
+User.create!(name: 'Admin2', email_address: 'admin2@example.com', password: 'test', password_confirmation: 'test',role: 'admin')\
+      .create_profile!(message: '管理者です。', github_id: 'ytnk531')\
+      .work_experiences.create(company_name: '毎朝新聞株式会社', work_start_on: Date.new)
+User.create!(name: 'User1', email_address: 'user1@example.com', password: 'test', password_confirmation: 'test',role: 'user')\
+      .create_profile!(message: 'ユーザーです。', github_id: 'ytnk531')\
+      .work_experiences.create(company_name: '毎朝新聞株式会社', work_start_on: Date.new)
+User.create!(name: 'User2', email_address: 'user2@example.com', password: 'test', password_confirmation: 'test',role: 'user')\
+      .create_profile!(message: 'ユーザーです。', github_id: 'ytnk531')\
+      .work_experiences.create(company_name: '毎朝新聞株式会社', work_start_on: Date.new)
+User.create!(name: 'User3', email_address: 'user3@example.com', password: 'test', password_confirmation: 'test',role: 'user')\
+      .create_profile!(message: 'ユーザーです。', github_id: 'ytnk531')\
+      .work_experiences.create(company_name: '毎朝新聞株式会社', work_start_on: Date.new)
+User.create!(name: 'User4', email_address: 'user4@example.com', password: 'test', password_confirmation: 'test',role: 'user')\
+      .create_profile!(message: 'ユーザーです。', github_id: 'ytnk531')\
+      .work_experiences.create(company_name: '毎朝新聞株式会社', work_start_on: Date.new)
 
